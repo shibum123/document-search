6eced75555001556092ed339e3ae16fb0f115278
Shibu Manoharan <shibu@mail.example.com>
2020-02-10 12:19:10 +0000

    react hooks integrated

--- src/components/SearchOverview.jsx
+++ src/components/SearchOverview.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createRef } from "react";
 import Datatable from "react-bs-datatable";
 import "../App.scss";
 import "font-awesome/css/font-awesome.min.css";
@@ -9,106 +9,103 @@ const indexName = "doc-index-01";
 let payload;
 
 const MyComponent = () => {
-
-  const [dataList, setDataList] = useState([])
+  const [dataList, setDataList] = useState([]);
+  const inputField = createRef();
 
   useEffect(() => {
-    getElasticSearchData();
-  })
+    getElasticSearchData();alert('hi')
+  }, []);
 
-  getElasticSearchData = (payload = "") => {
+  const getElasticSearchData = (payload = "") => {
     client
       .search({
         index: indexName,
         body: payload
       })
       .then(
-      function (resp) {
-        console.log(resp.hits);
-        setDataList(resp.hits.hits);
-      },
-      function (err) {
-        console.log(err.message);
-      }
+        function(resp) {
+          // console.log(resp.hits);
+          setDataList(resp.hits.hits);
+        },
+        function(err) {
+          // console.log(err.message);
+        }
       );
   };
-  searchItems = val => {
+  const searchItems = val => {
     payload = val
       ? {
-        query: {
-          regexp: {
-            title: {
-              value: ".*" + val + ".*",
-              flags: "ALL"
+          query: {
+            regexp: {
+              title: {
+                value: ".*" + val + ".*",
+                flags: "ALL"
+              }
             }
           }
         }
-      }
       : "";
     getElasticSearchData(payload);
   };
-  handleMouseClick = () => {
-    searchItems(refs["inputField"].value);
+  const handleMouseClick = () => {
+    searchItems(inputField.value);
   };
-  handleKeyDown = evt => {
+  const handleKeyDown = evt => {
     if (evt.key === "Enter") {
       searchItems(evt.target.value);
     }
   };
 
   const header = [
-    { title: 'File Name', prop: 'filename', sortable: true },
-    { title: 'Category', prop: 'category' },
-    { title: 'Submitted On', prop: 'create_date' },
-    { title: 'Perview', prop: 'content' },
-    { title: 'Action', prop: 'action' }
+    { title: "File Name", prop: "filename", sortable: true },
+    { title: "Category", prop: "category" },
+    { title: "Submitted On", prop: "create_date" },
+    { title: "Perview", prop: "content" },
+    { title: "Action", prop: "action" }
   ];
 
   const body = dataList.map(item => {
     return {
       filename: item._source.filename,
-      category: 'Submission',
+      category: "Submission",
       create_date: moment(item._source.create_date).format("DD-MM-YYYY"),
       content: `${item._source.content}...`,
-      action: <a href='#'>Go to</a>
-    }
-  })
+      action: <a href="#">Go to</a>
+    };
+  });
 
   return (
     <div className="search-overview">
       <div className="row justify-content-md-center">
-        <div className="col-md-8">
-          <center>
-            <h3 className="main-title">The Search Tool | V1.0</h3>
-          </center>
-          <div className="input-group search-overview__header">
-            <input
-              type="text"
-              className="form-control"
-              ref="inputField"
-              placeholder="Enter Search Text"
-              onKeyDown={handleKeyDown}
-              aria-label="Enter Search Text"
-              aria-describedby="basic-addon2"
-            />
-            <button
-              className="search-overview__input input-group-append"
-              onClick={handleMouseClick}
-            >
-              <i className="fa fa-search" aria-hidden="true"></i>
-            </button>
-            <input
-              type="button"
-              className="search-overview__button"
-              value="Search"
-              onClick={handleMouseClick}
-            />
-            <label className="search-overview__label">Advanced Search</label>
-          </div>
+        <center>
+          <h3 className="main-title">The Search Tool | V1.0</h3>
+        </center>
+        <div className="input-group search-overview__header">
+          <input
+            type="text"
+            className="form-control"
+            ref={inputField}
+            placeholder="Enter Search Text"
+            onKeyDown={handleKeyDown}
+            aria-label="Enter Search Text"
+            aria-describedby="basic-addon2"
+          />
+          <button
+            className="search-overview__input input-group-append"
+            onClick={handleMouseClick}
+          >
+            <i className="fa fa-search" aria-hidden="true"></i>
+          </button>
+          <input
+            type="button"
+            className="search-overview__button"
+            value="Search"
+            onClick={handleMouseClick}
+          />
+          <label className="search-overview__label">Advanced Search</label>
         </div>
       </div>
-      <div className="table-responsive">
-      </div>
+      <div className="table-responsive"></div>
       <Datatable
         tableHeaders={header}
         tableBody={body}
@@ -116,6 +113,6 @@ const MyComponent = () => {
       />
     </div>
   );
-}
+};
 
 export default MyComponent;
